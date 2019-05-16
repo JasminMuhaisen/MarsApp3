588e8498f3257cbf251614df02fbb8db9ec68def
Jasmin <yasmeenmohaisen@gmail.com>
2019-05-16 14:18:06 +0300

    deftha jdeed

--- app/src/main/java/com/example/marsapp/FriendsActivity.java
+++ app/src/main/java/com/example/marsapp/FriendsActivity.java
@@ -2,12 +2,55 @@ package com.example.marsapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
 
 public class FriendsActivity extends AppCompatActivity {
+
+    private RecyclerView myFriendList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_friends);
+
+        myFriendList = (RecyclerView)findViewById(R.id.friend_list);
+        myFriendList.setHasFixedSize(true);
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(  this);
+        linearLayoutManager.setReverseLayout (true);
+        linearLayoutManager.setStackFromEnd (true);
+        myFriendList.setLayoutManager(linearLayoutManager);
+
+        DisplayAllFriends ();
     }
+
+    private void DisplayAllFriends () {
+        FirebaseRecyclerAdapter<Friends, FriendsViewHolder> firebaseRecyclerAdapter
+                = new FirebaseRecyclerAdapter<Friends, FriendsViewHolder>
+                (
+                        Friends.class,
+                        )
+
+    }
+
+    public static class FriendsViewHolder extends RecyclerView.ViewHolder {
+        View mView;
+
+        public FriendsViewHolder(View itemView) {
+            super(itemView);
+            mView = itemView;
+
+
+
+        }
+
+
+
+        }
+
+    }
+
 }

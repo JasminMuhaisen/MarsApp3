d7a594532109babb4dd2057ecd67a8598652b5bd
Jasmin <yasmeenmohaisen@gmail.com>
2019-05-16 18:30:09 +0300

    final edit without any error

--- app/src/main/java/com/example/marsapp/SettingsActivity.java
+++ app/src/main/java/com/example/marsapp/SettingsActivity.java
@@ -55,7 +55,7 @@ public class SettingsActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_setting);
+        setContentView(R.layout.activity_settings);
 
         mAuth = FirebaseAuth.getInstance();
         currentUserId = mAuth.getCurrentUser().getUid();

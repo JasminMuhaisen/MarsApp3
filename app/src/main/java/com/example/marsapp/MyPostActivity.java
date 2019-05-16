27cf83ae072bfadb9608988a656bd3c7f8ff5d6e
Jasmin <yasmeenmohaisen@gmail.com>
2019-05-17 02:30:58 +0300

    modify

--- app/src/main/java/com/example/marsapp/MyPostActivity.java
+++ app/src/main/java/com/example/marsapp/MyPostActivity.java
@@ -2,12 +2,21 @@ package com.example.marsapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 public class MyPostActivity extends AppCompatActivity {
+
+    private Toolbar mToolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_post);
+
+        mToolbar = (Toolbar) findViewById(R.id.my_posts_bar_layout);
+        setSupportActionBar(mToolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setTitle("My posts");
     }
 }
